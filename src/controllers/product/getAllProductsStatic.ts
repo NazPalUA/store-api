f3431fa591f0cdaaa7bea09e610d7a9e6d2bcccc
Nazar Palamarchuk <ua.nazar.palamarchuk.ua@gmail.com>
2024-12-21 13:53:57 +0200

    Remove unused getAllProductsStatic controller file to streamline codebase

--- src/controllers/product/getAllProductsStatic.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express';
-
-const getAllProductsStatic = async (req: Request, res: Response) => {
-  res.status(200).json({ msg: 'Products Route Static' });
-};
-
-export { getAllProductsStatic };
